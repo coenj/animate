e90c45a067ef6620f828764a3827729d0a770291
Coen <coenc2000@hotmail.com>
2018-03-10 12:18:57 +0100

    Refactoring and initPoints inside the shape.

--- js/drawpoints.js
+++ js/drawpoints.js
@@ -1,3 +1,4 @@
+// author: CoenJanssen.net
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var raf;
@@ -5,30 +6,8 @@ var running = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var ratio = canvas.width / canvas.height;
-var linePoints = [];
+//var linePoints = [];
 var shapes = [];
-
-function Shape() {
-
-}
-
-function Shape(fill, stroke, x, y, linePoints) {
-    this.fill = fill;
-    this.stroke = stroke;
-    this.linePoints = this.linePoints;
-    this.x = x;
-    this.y = y;
-}
-
-
-Shape.prototype = {
-    linePoints: [],
-    fill: 'green',
-    stroke: 'orange',
-    x: 10,
-    y: 10,
-    linePoints: []
-}
 
 function linePoint() {
 
@@ -44,38 +23,46 @@ linePoint.prototype = {
     y: 10,
     speedX: 4,
     speedY: 4,
-    inCanvasY: function () { if (this.y + this.speedY > canvas.height || this.y + this.speedY < 0) this.speedY = -this.speedY },
-    inCanvasX: function () { if (this.x + this.speedX > canvas.width || this.x + this.speedX < 0) this.speedX = -this.speedX },
-
-    moveX: function () {
-        this.x += this.speedX
+    update: function () {
+        if (this.x + this.speedX > canvas.width || this.x + this.speedX < 0) this.speedX = -this.speedX
+        if (this.y + this.speedY > canvas.height || this.y + this.speedY < 0) this.speedY = -this.speedY
     },
+    moveX: function () { this.x += this.speedX },
     moveY: function () { this.y += this.speedY },
-
-    /*draw: function () {
-        ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x + 10, this.y + 10);
-        ctx.closePath();
-        ctx.fillStyle = this.color;
-        ctx.strokeStyle = 'green';
-        ctx.stroke();
-        ctx.fill();
-    }
-    */
 }
 
 function initPoints() {
     var array = [];
 
     for (x = 0; x < canvas.width; x = x + 30) {
-
-        //linePoints.push(new linePoint(x, x - x / ratio));
         pos = Math.random() * canvas.width;
         array.push(new linePoint(pos, pos));
-        }
-        return array;
+    }
+
+    return array;
 }
+
+function Shape() {
+
+}
+
+function Shape(fill, stroke, x, y, //linePoints
+) {
+    this.fill = fill;
+    this.stroke = stroke;
+   // this.linePoints = this.linePoints;
+    this.x = x;
+    this.y = y;
+}
+
+Shape.prototype = {
+    linePoints: [] = initPoints(),
+    fill: 'green',
+    stroke: 'orange',
+    x: 10,
+    y: 10,
+}
+
 
 function clear() {
     ctx.fillStyle = 'white';
@@ -83,20 +70,17 @@ function clear() {
 }
 
 function drawLines(point) {
-    point.inCanvasX();
-    point.inCanvasY();
+    point.update();
     point.moveX();
     point.moveY();
     ctx.lineTo(point.x, point.y);
 }
 
-
 function draw(shape) {
     //clear();
-
     ctx.beginPath();
     ctx.moveTo(shape.x, shape.y);
-o=shape;
+    o = shape;
     o.linePoints.forEach(drawLines);
     ctx.closePath();
 
@@ -104,45 +88,25 @@ o=shape;
     ctx.strokeStyle = shape.stroke;
     ctx.stroke();
     ctx.fill();
-
-    
 }
 
-function animate(){
-    
-    shapes.forEach(draw);    
- 
+function animate() {
+    shapes.forEach(draw);
     raf = window.requestAnimationFrame(animate);
 }
 
-var points = [];
-points=initPoints();
-shape = new Shape(`rgba(128,0, 0, 0.5)`, 'blue',0, 20);
-shape.linePoints=points;
+
+shape = new Shape(`rgba(128,0, 0, 0.5)`, 'blue', 0, 20);
+
 shapes.push(shape);
 
-shape = new Shape(`rgba(0,128, 128, 0.5)`, 'green',600, 400);
-points=initPoints();
-points.push(new linePoint(100,800));
-shape.linePoints=points;
+shape = new Shape(`rgba(0,128, 128, 0.5)`, 'green', 600, 400);
 
 shapes.push(shape);
 
 animate(shapes);
 
-
-
 /*
-function drawInPlace(x, y) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + 10, y + 10);
-    ctx.closePath();
-    ctx.fillStyle = 'red';
-    ctx.strokeStyle = 'orange';
-    ctx.stroke();
-    ctx.fill();
-}
 canvas.addEventListener('mousemove', function (e) {
     if (!running) {
         clear();
